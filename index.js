7e2e324b79b98fb76a8aab4bfcdb8c1497916d60
EsdrasMukuku <esdrasbam0@gmail.com>
2024-02-22 14:44:17 -0600

    added finished files

---
+++ index.js
@@ -0,0 +1,25 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+
+let btnGen = document.getElementById("btn");
+let pass1  = document.getElementById("pass1");
+let pass2  = document.getElementById("pass2");
+
+
+function generatePassword(length) {
+    let password = '';
+    for (let i = 0; i < length; i++) {
+      const randomIndex = Math.floor(Math.random() * characters.length);
+      password += characters[randomIndex];
+    }
+    return password;
+  }
+
+  btnGen.addEventListener('click', function(){
+    const password1 = generatePassword(15);
+    const password2 = generatePassword(15);
+
+    pass1.textContent = password1;
+    pass2.textContent = password2;
+
+  });
